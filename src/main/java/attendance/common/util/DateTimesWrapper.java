ef3b43a51559bf23b74c8ba5c8dc24ec4d5051e3
황재웅 <wodnd0131@naver.com>
2024-12-14 13:26:36 +0900

    feat: 유틸리티 래퍼 클래스 구현
    - DateTime 래퍼 클래스 추가

---
+++ src/main/java/attendance/common/util/DateTimesWrapper.java
@@ -0,0 +1,29 @@
+package attendance.common.util;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import camp.nextstep.edu.missionutils.DateTimes;
+
+public final class DateTimesWrapper {
+    private static final String DATE_PATTERN = "yyyy-MM-dd";
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(DATE_PATTERN);
+
+    private DateTimesWrapper() {
+        throw new AssertionError();
+    }
+
+    public static LocalDateTime now() {
+        return DateTimes.now();
+    }
+
+    public static String nowToString() {
+        return now().format(DATE_FORMATTER);
+    }
+
+    public static LocalDateTime toLocalDateTime(String dateStr) {
+        LocalDate date = LocalDate.parse(dateStr, DATE_FORMATTER);
+        return date.atStartOfDay();  // 시간을 00:00:00으로 설정
+    }
+}
